f0076309aaf2de438df3c617dc9f59ffa93da7bc
Sithief <gojkljkl@gmail.com>
2019-11-30 17:07:00 +0700

    add delete button

--- vk_api/friendlist.js
+++ vk_api/friendlist.js
@@ -1,14 +1,16 @@
 var response_div;
+var to_delete = [];
 
 function setup() {
     readCookie();
     get_deleted_friends_button = createButton('Найти "собачек"');
-    get_deleted_friends_button.mousePressed(getDeleted);    
-    response_div = createP('Удалённые страницы:');
+    get_deleted_friends_button.position(0);
+    get_deleted_friends_button.mousePressed(getDeleted);
 }
 
 function getDeleted() {
-  request(print_result, 'friends.get', {'fields':'last_seen,photo_100'});
+    to_delete = [];
+    request(print_result, 'friends.get', {'fields':'last_seen,photo_100'});
 }
 
 function print_result(response) {
@@ -16,6 +18,10 @@ function print_result(response) {
         return;
     }
     print(response);
+    if (response_div){
+        response_div.remove();
+    }
+    response_div = createP('Удалённые страницы:')
     let items = response.response.items;
     for (let i = 0; i < items.length/5; i++) {
         if (items[i].deactivated) {
@@ -25,8 +31,21 @@ function print_result(response) {
             let name = items[i].first_name + ' ' + items[i].last_name;
             let user_name = createA(url, name);
             let user_photo = createImg(items[i].photo_100, '');
+            let delete_button = createButton('Удалить', items[i].id);
+            delete_button.mousePressed(deleteBlocked);
             user_div.child(user_photo);
             user_div.child(user_name);
+            user_div.child(delete_button);
         }
     }
+}
+
+function deleteBlocked() {
+    let user_id = this.value();
+    request(reloadDeletedList, 'friends.delete', {'user_id': user_id});
+}
+
+function reloadDeletedList(response) {
+    print(response);
+    getDeleted();
 }

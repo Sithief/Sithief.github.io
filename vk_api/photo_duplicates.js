1af05cecd2151ae995ae4aab2459e66078f22a84
Sithief <gojkljkl@gmail.com>
2019-12-12 03:24:32 +0700

    add functions to fing duplicates

--- vk_api/photo_duplicates.js
+++ vk_api/photo_duplicates.js
@@ -2,6 +2,8 @@ var albums_div;
 var images_div;
 var albums = {};
 var process_bar = {};
+var duplicates = [];
+var accuracy = 5;
 
 function setup() {
     readCookie();
@@ -66,26 +68,99 @@ function appendAlbumsPhotos(response) {
             if (a.height > b.height) { return b;} 
             else {return a;}
         });
-        var img = loadImage(min_size.url);
+        item.img = loadImage(min_size.url, successLoad);
+        item.img.album_id = item.album_id;
         albums['album_id='+item.album_id].push(item);
-        let progress = Math.floor(100 * albums['album_id='+item.album_id].length / response.response.count);
-        process_bar['album_id='+item.album_id].html(progress + '%');
     }
     if (response.response.count > albums['album_id='+album_id].length) {
         request(appendAlbumsPhotos, 'photos.get', {'album_id': album_id, 'count': 1000, 'offset': albums['album_id='+album_id].length});
     }
 }
 
+function successLoad() {
+    //this.loadPixels();
+    let alb = albums['album_id='+this.album_id];
+    let success = alb.filter(a => a.img.height > 1);
+    print(success);
+    let progress = Math.floor(map(success.length, 0, alb.length, 0, 100));
+    process_bar['album_id='+this.album_id].html(progress + '%');
+}
 
 function searchDuplicates() {
     if (albums_div){
         albums_div.remove();
     }
+    var photos = [];
+    for (let a in albums) {
+        for (let p in albums[a]) {
+            photos.push(albums[a][p]);
+        }
+    } 
+    //process_bar = createCanvas(300, 300);
+    //background(45);
+    //fill(200);  
+    print(photos);
+    let photos_count = photos.length;
+    while (photos.length > 0) {
+        //rect(0, 0, map(photos.length, photos_count, 0, 0, width), height);
+        let act_ph = photos.pop();
+        //image(act_ph.img, photos.length * 100, 0);
+        let similar = comparePhotoWithOther(act_ph, photos);
+        if (similar.length > 1) {
+            showDuplicates(similar);
+        }      
+        print(map(photos.length, photos_count, 0, 0, 100) + '%', similar);
+    }
 }
 
+function comparePhotoWithOther(photo, other_photos) {
+    var similar_list = [photo];
+    photo.img.loadPixels();
+    let photo_pixels = photo.img.pixels;
+    photo.img.updatePixels();
+    for (let i in other_photos) {
+        let ophoto = other_photos[i];
+        ophoto.img.loadPixels();
+        let ophoto_pixels = ophoto.img.pixels;
+        ophoto.img.updatePixels();
+        //print(photo.img.height + 'x' + photo.img.width, ophoto.img.height + 'x' + ophoto.img.width)
+        if (photo.img.height == ophoto.img.height & photo.img.width == ophoto.img.width) {
+            var similar = true;
+            //print(photo_pixels[0], ophoto_pixels[0], similar);
+            for (let pix = 0; pix < 4 * photo.img.height * photo.img.width; pix++) {
+                if (abs(photo_pixels[pix] - ophoto_pixels[pix]) > accuracy) {
+                    similar = false;
+                    break;
+                }
+                //print(photo_pixels[pix], ophoto_pixels[pix], similar, photo, ophoto);
+            }
+            if (similar) {
+                similar_list.push(ophoto);
+            }
+        }
+    }
+    return similar_list;
+}
 
+function showDuplicates(similar_list) {
+    var dupl_div = createDiv();
+    for (let i in similar_list) {
+        let photo = similar_list[i];
+        let size = photo.sizes.reduce(function (a, b) {
+        if (a.height < b.height) { return a;} 
+            else {return b;}
+        });
+        print(size);
+        var dupl_img = createImg(size.url);
+        dupl_img.href = 'https://vk.com/photo'+ photo.owner_id +'_' + photo.id;  
+        dupl_img.mouseClicked(openVkImage);    
+        dupl_img.parent(dupl_div);
+    }
+}
 
-
+function openVkImage() {
+    window.open(this.href);
+}
 
 
 
